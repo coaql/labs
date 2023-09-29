cd7a5bb232abec69563dda9d553e906ca5d68935
vikas @ uranus <vikass21ite@student.mes.ac.in>
2023-09-29 17:08:17 +0530

    menujs

--- sem5/ip/index.js
+++ sem5/ip/index.js
@@ -1,5 +1,32 @@
 //dynamic menu listing 
+menu = {
+	"meals":[
+	{id:1, name:"tea", con:0, rate:12},
+	{id:2, name:"coffee", con:0, rate:12},
+	{id:3, name:"latte", con:0, rate:12}
+	]
+};
+
+for(i = 0, n = menu.meals; i < n.length; i++)
+{
+
+  const ob = menu.meals[i];
+  const tr = document.createElement("tr");
+
+  for(prop in ob)
+  {
+    d = document.createTextNode(ob[prop]);
+    const td = document.createElement("td");
+    td.appendChild(d);
+    tr.appendChild(td);
+  }
+  document.getElementById("meals").appendChild(tr);
+
+}
+  
+
 //button to add to cart
 //cart
 //dynamic trending items 
 //filters
+//fancy
